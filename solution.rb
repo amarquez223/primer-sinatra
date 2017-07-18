67911ce60f41e3b1125be4d236ec38501ee49227
amarquez223 <anderson.marquez@gmail.com>
2017-07-18 15:39:27 -0500

    Saludame 3

--- solution.rb
+++ solution.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
 
-get '/makers/:nombre' do
-	"<h1>" + "Hola #{params[:nombre].capitalize}" + "</h1>"
+get '/' do
+	<<-HTML
+	<h1>Hola dime a quien saludar</h1>
+	<form action="/views" method="post">
+		<input type="text" name="nombre"></input>
+		<input type="submit" value="Submit"></input>
+	</form>
+	HTML
+end
+
+post '/views' do
+	"¡Hola #{params[:nombre]}!"
 end
