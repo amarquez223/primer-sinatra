111ba19997167bd8995b33c4df7433a92b81f203
amarquez223 <anderson.marquez@gmail.com>
2017-07-18 15:45:42 -0500

    El Código Secreto

--- solution.rb
+++ solution.rb
@@ -1,15 +1,5 @@
 require 'sinatra'
 
 get '/' do
-	<<-HTML
-	<h1>Hola dime a quien saludar</h1>
-	<form action="/views" method="post">
-		<input type="text" name="nombre"></input>
-		<input type="submit" value="Submit"></input>
-	</form>
-	HTML
-end
-
-post '/views' do
-	"¡Hola #{params[:nombre]}!"
+	"HcUy6Re2LLBRtj"
 end
